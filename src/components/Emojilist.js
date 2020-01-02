a7e14b3eaaceca4d3e83c8e3b038ce96e9682b93
Rupasharma18 <rupa18@navgurukul.org>
2020-01-02 11:52:04 +0530

    update files

--- src/components/Emojilist.js
+++ src/components/Emojilist.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Emoji from './Emoji.json';
 
+
 function searchEmoji(search){
 	return (data) => {
         // console.log(data)
-		return data.keywords.toLowerCase().includes(search.toLowerCase()) ;
+        // return data.keywords.toLowerCase().includes(search.toLowerCase()) ;
+        if (data.title.toLowerCase().includes(search.toLowerCase())) {
+            return true;
+            }
+        if (data.keywords.includes(search)) {
+        return true;
+        }
+        return false;
 	}
 }
 
 class EmojiList extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {jsondata:Emoji, search: '', copySuccess: ''}
+    constructor(){
+        super();
+        this.state = {jsondata:Emoji, search: '',copySuccess: ''}
     }
 // this method for updateing input
 changeInput(){
@@ -19,28 +27,31 @@ changeInput(){
         search:this.searchEmoji.value
     })
 }
-// this method for copy cilpboard
-copyToClipboard(e){
-    this.li.select();
-    document.execCommand('copy');
-    e.target.focus();
-    this.setState({ copySuccess: 'Copied!' });
-  };
 
-    render() {
+copyToClipboard = (symbol) => {
+    console.log('text', symbol)
+    var textField = document.createElement('textarea')
+    textField.innerText = symbol
+    document.body.appendChild(textField)
+    textField.select()
+    document.execCommand('copy')
+    textField.remove()
+  }
+  render() {
            const emojiList = this.state.jsondata.filter(searchEmoji(this.state.search)).slice(0, 20).map(emoji =>
-            <div className="list"> <li className="emoji-list"> {emoji.symbol} {emoji.title}</li></div>) 
-            // console.log(emojiList)
+            <div className="list"> 
+            <li className="emoji-list">{emoji.symbol}  <span className="emoji-title">{emoji.title}</span> 
+                {this.state.copySuccess}
+                <span className="copys" onClick={()=>this.copyToClipboard(emoji.symbol)}>Click to Copy</span>
+            </li>
+            </div>) 
             
         return(
-        
             <div className="search-input">
                <form>
                <input placeholder="Search..." ref={input => this.searchEmoji = input} onChange={this.changeInput.bind(this)} className="input" />
                </form>
                <div className="searchEmoji">{emojiList}</div>
-               
-               {/* {this.state.copySuccess} */}
             </div> 
            
         )
--- src/components/forkRibbon.js
+++ src/components/forkRibbon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Ribbon(){
     return (
-        <a class="github-fork-ribbon" href="https://github.com/ahfarmer/emoji-search" title="Fork me on GitHub">Fork me on GitHub</a>
+        <a class="github-fork-ribbon" href="https://github.com/Rupasharma18/Emoji-search" title="Fork me on GitHub">Fork me on GitHub</a>
       
     )
 }
